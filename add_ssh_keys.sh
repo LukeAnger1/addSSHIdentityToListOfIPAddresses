0557110fb58bfdaf89392de243e640ae0b960658
Luke Anger <anger.luke2004@gmail.com>
2024-02-13 23:56:00 -0500

    I added a timeout feature so it doesnt take so frickin long

--- add_ssh_keys.sh
+++ add_ssh_keys.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Define timeout value (in seconds)
+TIMEOUT=2
+
 # Check if the user has provided a file name
 if [ -z "$1" ]; then
     echo "Usage: $0 <file>"
@@ -14,6 +17,6 @@ fi
 
 # Read each line (IP address) from the file
 while IFS= read -r ip_address; do
-    # Attempt to add SSH key to the remote host
-    ssh-copy-id -i ~/.ssh/id_rsa.pub "$ip_address"
+    # Attempt to add SSH key to the remote host with timeout
+    timeout $TIMEOUT ssh-copy-id -i ~/.ssh/id_rsa.pub "$ip_address"
 done < "$1"
